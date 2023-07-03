3fb3811a40f17d9faedef2fa2f2b7761135cc154
huda elhady <huda.m.elhady@gmail.com>
2023-07-03 19:36:15 +0300

    rename package from ReactNativeDemo2 to SportbuffReactNativeDemo

--- ReactNativeDemo2/ios/SportbuffReactNativeDemoTests/SportbuffReactNativeDemoTests.m
+++ ReactNativeDemo2/ios/SportbuffReactNativeDemoTests/SportbuffReactNativeDemoTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface ReactNativeDemo2Tests : XCTestCase
+@interface SportbuffReactNativeDemoTests : XCTestCase
 
 @end
 
-@implementation ReactNativeDemo2Tests
+@implementation SportbuffReactNativeDemoTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
